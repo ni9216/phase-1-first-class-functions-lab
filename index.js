d101ff93a9a84be7f315400325ac81ad1f13b108
ni9216 <nicholasliech851@gmail.com>
2024-07-04 22:27:27 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function createFareMultiplier(multiplier){
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+
+function selectDifferentDrivers(drivers, driverSelector){
+    return driverSelector(drivers);
+}
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+consolelog(selectDifferentDrivers(driver, returnFirstTwoDrivers));
+consolelog(selectDifferentDrivers(driver, returnLastTwoDrivers));
+
+console.log(fareDoubler(10));
+console.log(fareTripler(10));
